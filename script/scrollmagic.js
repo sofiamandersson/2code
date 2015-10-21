26812a5a55c92ee9c98a113414e93a61ef6c2fde
Sofia Andersson <sofia.m.andersson.a@gmail.com>
2015-10-21 19:32:58 +0200

    Generall fixings

---
+++ script/scrollmagic.js
@@ -0,0 +1,18 @@
+var controller = new ScrollMagic.Controller();
+			$(function () {
+				var scene = new ScrollMagic.Scene({triggerElement: "#morestart1", duration: 6800})
+								.setPin("#morestay1")
+								.addTo(controller);
+				
+				var scene = new ScrollMagic.Scene({triggerElement: "#morestart2", duration: 2100})
+								.setPin("#morestay2")
+								.addTo(controller);
+				
+				var scene = new ScrollMagic.Scene({triggerElement: "#morestart3", duration: 2100, offset: 200})
+								.setPin("#morestay3")
+								.addTo(controller);
+				var scene = new ScrollMagic.Scene({triggerElement: "#morestart4", duration: 2100, offset: 200})
+								.setPin("#morestay4")
+								.addTo(controller);
+				
+			});
